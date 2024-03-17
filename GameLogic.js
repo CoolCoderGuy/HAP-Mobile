5d482871593c1ac0323ed22835dd218581f44262
CoolCoderGuy <158845593+CoolCoderGuy@users.noreply.github.com>
2024-03-16 19:17:42 -0500

    Update GameLogic.js

--- GameLogic.js
+++ GameLogic.js
@@ -146,17 +146,9 @@ if(score == 100){
             mobileHighScore = score; // Replace high score with current score if current score is higher
             localStorage.setItem('mobileHighScore', mobileHighScore); // Save updated high score to localStorage
         }
-
-sjs.onHit("ball",["top_screen"], function(){ 
-window.location = "gameover.html";
+        window.location = "gameover.html";
     }); 
 
-sjs.onHit("ball",["bottom_screen"], function(){ 
-window.location = "Congratulations.html";
-    }); 
-
-    });
- 
     // The few keyboard controls left in the mobile version
     sjs.keyDown(DOWN_KEY, function(){ 
         ball.friction -= -0.001;
